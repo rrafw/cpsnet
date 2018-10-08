26a89cf6445d209a1fa8378cb5f0fab26123e7f0
Rodrigo Rodrigues <rrafwbb@hotmail.com>
2018-10-08 16:49:08 -0300

    second commit

--- cpsnetFetch/spiders/cpsnetFetch.py
+++ cpsnetFetch/spiders/cpsnetFetch.py
@@ -1,4 +1,6 @@
+import config
 import scrapy
+
 
 class cpsnetFetch(scrapy.Spider):
 
@@ -6,23 +8,19 @@ class cpsnetFetch(scrapy.Spider):
     allowed_domains = ['comprasnet.gov.br']
 
     date = '26/07/2018'
-    checkForm = 1
 
     def start_requests(self, date=date):
         #pagina = 40
-        
-        #while (checkForm["check"] == True):   
-        for pagina in range(24,30):
-            
-            global checkForm
-            return checkForm
-            if (checkForm == 1):
+        #while (checkForm == 1):   
+        for pagina in range(24,30):            
+
+            if (config.checkForm == False):
+                break
+            else:
                 url = 'http://comprasnet.gov.br/ConsultaLicitacoes/ConsLicitacao_Relacao.asp?dt_publ_ini='+ date +'&dt_publ_fim='+ date +'&chkModalidade=1,2,3,20,5,99&chk_concor=31,32,41,42&chk_pregao=1,2,3,4&chk_rdc=1,2,3,4&optTpPesqMat=M&optTpPesqServ=S&chkTodos=-1&chk_concorTodos=-1&chk_pregaoTodos=-1&numpag='+ str(pagina)
                 yield scrapy.Request(url=url, callback=self.parse, meta={'url': url, 'pagina': pagina})
                 print(pagina)
                 #pagina += 1
-            else:
-                break
         
             
     def parse(self, response, date=date):
@@ -36,8 +34,8 @@ class cpsnetFetch(scrapy.Spider):
             self.log('Saved file %s' % filename)
             print('SSSSSSSSSSSSSSSSSSSSSSSSSSS')
         else:
-            global checkForm
-            checkForm = 0
+            config.checkForm = False
             print('NNNNNNNNNNNNNNNNNNNNNNNNNN')
+
 
     
