f05a1c8bf9c3e8a0350bf78f18eda2f9c7d7813d
Rodrigo Rodrigues <rrafwbb@hotmail.com>
2018-10-09 15:09:26 -0300

    first working

--- cpsnetFetch/spiders/cpsnetFetch.py
+++ cpsnetFetch/spiders/cpsnetFetch.py
@@ -16,7 +16,6 @@ class cpsnetFetch(scrapy.Spider):
         while pagina < 40:   
         #for pagina in range(25,28):
             url = 'http://comprasnet.gov.br/ConsultaLicitacoes/ConsLicitacao_Relacao.asp?dt_publ_ini='+ date +'&dt_publ_fim='+ date +'&chkModalidade=1,2,3,20,5,99&chk_concor=31,32,41,42&chk_pregao=1,2,3,4&chk_rdc=1,2,3,4&optTpPesqMat=M&optTpPesqServ=S&chkTodos=-1&chk_concorTodos=-1&chk_pregaoTodos=-1&numpag='+ str(pagina)
-            yield scrapy.Request(url=url, callback=self.parse, meta={'url': url, 'pagina': pagina})
             page = urlopen(url).read()
             soup = BeautifulSoup(page, features="lxml")
             form = soup.find('form')
